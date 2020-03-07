22742b4397d6dc17690cc967f32b8ae711078e00
Carley Thompson <44657901+carleythomp@users.noreply.github.com>
2020-03-06 20:56:13 -0600

    Deleting Assets
    Didn't work

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] float speed = 2.0f;
+    [SerializeField] float rotateSpeed = 3.0f;
+    CharacterController cc;
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+    }
+
+   
+    void Update()
+    {
+        transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed, 0);
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        float curSpeed = speed * Input.GetAxis("Vertical");
+        cc.SimpleMove(forward * curSpeed);
+
+    }
+}
