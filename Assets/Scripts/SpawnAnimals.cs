27cbe20d085246c820c0281438470096eda17e7e
Carley Thompson <44657901+carleythomp@users.noreply.github.com>
2020-03-09 14:51:23 -0500

    Fixed SpawnAnimal
    Animals spawn randomly and correctly :)

--- Assets/Scripts/SpawnAnimals.cs
+++ Assets/Scripts/SpawnAnimals.cs
@@ -6,20 +6,54 @@ public class SpawnAnimals : MonoBehaviour
 {
     [SerializeField] GameObject[] animalPrefab;
     [SerializeField] bool stopSpawning = false;
-    [SerializeField] float spawnTime;
-    [SerializeField] float spawnDelay;
-
+    private float spawnIntervals = 8.0f;
+    private float spawnDelay = 10.0f;
+    private float spawnRangeX = 5;
+    private float spawnPosZ = 8;
+    private float xRange = 10;
+    private int count = 0;
     void Start()
     {
-        InvokeRepeating("MakingAnimals", 2.0f, 0.3f);
+       // InvokeRepeating("MakingAnimals", spawnDelay, spawnIntervals);
     }
+
+    void Update()
+    {
+       
+        if(spawnDelay <= 0f)
+        {
+            MakingAnimals();
+            spawnDelay = spawnIntervals;
+        }
+
+        spawnDelay -= Time.deltaTime;
+    }
+
 
     void MakingAnimals()
     {
-        Quaternion randomRotation = Random.rotationUniform;
-        Vector3 spawnPosition = transform.position + transform.forward * 0.5f;
-        int animalIndex = Random.Range(0, animalPrefab.Length);
-        Instantiate(animalPrefab[animalIndex], spawnPosition, randomRotation);
+        if(count < 5 )
+        {
+            int animalIndex = Random.Range(0, animalPrefab.Length);
+            //Quaternion randomRotation = Random.rotationUniform;
+            //Vector3 spawnPosition = transform.position + transform.forward * 0.5f;
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+            Instantiate(animalPrefab[animalIndex], spawnPos, animalPrefab[animalIndex].transform.rotation);
+
+
+            
+            //if (transform.position.x < -xRange)
+            //{
+            //    transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+            //}
+            //if (transform.position.x > xRange)
+            //{
+            //    transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+            //}
+           // Instantiate(animalPrefab[animalIndex], spawnPos, animalPrefab[animalIndex].transform.rotation);
+            count++;
+        }
+       
     }
 
 }
