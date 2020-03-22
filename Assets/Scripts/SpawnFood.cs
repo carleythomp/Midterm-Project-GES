b5afc984fe104f8ed6055dbbdbdabb3c838aed0e
Carley Thompson <44657901+carleythomp@users.noreply.github.com>
2020-03-22 18:21:25 -0500

    SpawnFood

--- Assets/Scripts/SpawnFood.cs
+++ Assets/Scripts/SpawnFood.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class SpawnFood : MonoBehaviour
 {
     [SerializeField] GameObject[] foodPrefabs;
-
+    private int count = 0;
+    private float spawnRangeX = 60;
+    private float spawnPosZ = 30;
     void Start()
     {
         
@@ -13,6 +15,23 @@ public class SpawnFood : MonoBehaviour
 
     void Update()
     {
-        
+        PlacingFood();
+
+
+    }
+
+    void PlacingFood()
+    {
+        if (count < 5)
+        {
+            GameObject foodPrefab = foodPrefabs[Random.Range(0, foodPrefabs.Length)];
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX / 2, spawnRangeX / 2), 0, spawnPosZ);
+            Instantiate<GameObject>(foodPrefab, spawnPos, transform.rotation);
+
+            //hmm OH make random z position??
+
+
+            count++;
+        }
     }
 }
