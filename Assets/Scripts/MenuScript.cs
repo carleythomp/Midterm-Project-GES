7eafd6d11e9438c302f581def6e564a2c9f921a2
Carley Thompson <44657901+carleythomp@users.noreply.github.com>
2020-03-30 13:13:07 -0500

    Created MainMenu

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    [SerializeField] GameObject mainMenu;
+    [SerializeField] GameObject creditMenu;
+    
+    public void StartGame()
+    {
+        //SceneManager.LoadScene("Level1");
+        mainMenu.SetActive(false);
+    }
+
+    public void ShowCredits()
+    {
+        mainMenu.SetActive(false);
+        creditMenu.SetActive(true);
+
+    }
+
+    public void ShowMainMenu()
+    {
+        mainMenu.SetActive(true);
+        creditMenu.SetActive(false);
+    }
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
